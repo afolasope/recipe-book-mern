a11202c3bc164bca71dc2b9f9c57cae9206b0e74
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2023-01-17 10:57:06 +0100

    sets up authentication forms

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Wrapper>
+      <form>
+        <p className="title">Login to access your account</p>
+        <div>
+          <p>Email:</p>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>
+          <p>Password:</p>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div>
+          <p>I don't have an account yet? <Link to='/signup'>Singup</Link></p>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
 
-export default Login
+export default Login;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 80px);
+  .title {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+  form {
+    background-color: #f2f2f2;
+    border-radius: 5px;
+    padding: 1.5rem;
+  }
+  form p {
+    color: #14171a;
+  }
+  input {
+    display: block;
+    width: 100%;
+    padding: 0.4rem 0.2rem;
+    font-family: inherit;
+    font-size: 0.8rem;
+    margin-bottom: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  a {
+    color: #f59382;
+  }
+`;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <Wrapper>
+      <form>
+        <p className="title">Create an account to get started</p>
+        <div>
+          <p>First Name:</p>
+          <input type="text" id="firstName" name="firstName" />
+        </div>
+        <div>
+          <p>Last Name:</p>
+          <input type="text" id="lastName" name="lastName" />
+        </div>
+        <div>
+          <p>Username:</p>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>
+          <p>Email:</p>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>
+          <p>Password:</p>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div>
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
 
-export default Signup
+export default Signup;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 80px);
+  .title {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+  form {
+    background-color: #f2f2f2;
+    border-radius: 5px;
+    padding: 1.5rem;
+  }
+  form p {
+    color: #14171a;
+  }
+  input {
+    display: block;
+    width: 100%;
+    padding: 0.4rem 0.2rem;
+    font-family: inherit;
+    font-size: 0.8rem;
+    margin-bottom: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  a {
+    color: #f59382;
+  }
+`;
